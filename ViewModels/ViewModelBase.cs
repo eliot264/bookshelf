13e379fc7d41aa091366f180b237ab7200f6d184
eliot264 <wk49439@zut.edu.pl>
2023-09-10 14:44:21 +0200

    Add ViewModelBase class

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshelf.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }     
+        }
+    }
+}
