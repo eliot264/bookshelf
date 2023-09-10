dac9cc2d19abd73570b72424ca8c6eb27dc66b00
eliot264 <wk49439@zut.edu.pl>
2023-09-10 14:48:37 +0200

    Add MainViewModel Class

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshelf.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel => _currentViewModel;
+
+        public MainViewModel(ViewModelBase currentViewModel)
+        {
+            _currentViewModel = currentViewModel;
+        }
+    }
+}
